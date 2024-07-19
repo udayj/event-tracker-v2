02557c8fad1fe723614dab2b6800b9247a3cb1a4
udayzkx <uday@zkx.fi>
2024-07-19 08:44:26 +0000

    dynamic block numbers, network configuration

---
+++ src/config.rs
@@ -0,0 +1,128 @@
+use clap::{App, Arg};
+use config::{Config as C, ConfigError, File};
+use serde::Deserialize;
+pub struct Config {
+    pub from_block: u32,
+    pub to_block: u32,
+    pub starknet_rpc_url: String,
+    pub eth_rpc_url: String,
+    pub l2_sender: String,
+    pub starkway_l1: String,
+    pub starkway_l2: String,
+    pub starknet_core: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct NetworkConfig {
+    pub starknet_rpc_url: String,
+    pub eth_rpc_url: String,
+    pub starkway_l1: String,
+    pub starkway_l2: String,
+    pub starknet_core: String,
+}
+
+fn load_config(network: String) -> Result<NetworkConfig, ConfigError> {
+    let settings = C::builder()
+        .add_source(File::with_name((network + ".yaml").as_str()))
+        .build()?;
+    let network_config: NetworkConfig = settings.try_deserialize()?;
+    Ok(network_config)
+}
+
+pub fn get_config() -> Result<Config, Box<dyn std::error::Error>> {
+    let matches = App::new("Event Tracker")
+        .version("0.1.0")
+        .author("Uday <uday@zkx.fi>")
+        .about("Track Starkway Events")
+        .arg(
+            Arg::with_name("from_block")
+                .short("from")
+                .long("from-block")
+                .help("Get events from this block number")
+                .takes_value(true)
+                .validator(|val| match val.parse::<u32>().is_ok() {
+                    true => Ok(()),
+                    _ => Err(String::from("unexpected value of From-block number")),
+                })
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("to_block")
+                .short("to")
+                .long("to-block")
+                .help("Get events till this block number")
+                .takes_value(true)
+                .validator(|val| match val.parse::<u32>().is_ok() {
+                    true => Ok(()),
+                    _ => Err(String::from("unexpected value of To-block number")),
+                })
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("network")
+                .short("n")
+                .long("network")
+                .help("Retrieve events for this network sepolia/mainnet")
+                .takes_value(true)
+                .default_value("mainnet")
+                .required(false),
+        )
+        .arg(
+            Arg::with_name("l2_sender")
+                .short("s")
+                .long("sender")
+                .help("Sets L2 sender address as a filter for the events")
+                .takes_value(true)
+                .required(false),
+        )
+        .get_matches();
+
+    let from_block = matches
+        .value_of("from_block")
+        .map(|val| val.parse::<u32>().unwrap())
+        .unwrap();
+    let to_block = matches
+        .value_of("to_block")
+        .map(|val| val.parse::<u32>().unwrap())
+        .unwrap();
+    let network = matches.value_of("network").unwrap();
+    let network_config = load_config(network.to_string())?;
+    let l2_sender = if matches.is_present("l2_sender") {
+        matches.value_of("l2_sender").unwrap()
+    } else {
+        ""
+    };
+
+    let formatted_l2_sender = if matches.is_present("l2_sender") {
+        matches.value_of("l2_sender").unwrap()
+    } else {
+        "NO L2 SENDER SPECIFIED"
+    };
+
+    println!("From Block:{}", from_block);
+    println!("To Block:{}", to_block);
+    println!("Network:{}", network);
+    println!(
+        "Starknet RPC URL:{}",
+        network_config.starknet_rpc_url.clone()
+    );
+    println!("Ethereum RPC URL:{}", network_config.eth_rpc_url.clone());
+    println!("Starkway L1 Address:{}", network_config.starkway_l1.clone());
+    println!("Starkway L2 Address:{}", network_config.starkway_l2.clone());
+    println!(
+        "Starknet Core Address:{}",
+        network_config.starknet_core.clone()
+    );
+    println!("L2 Sender:{}", formatted_l2_sender);
+
+    Ok(Config {
+        from_block,
+        to_block,
+        starknet_rpc_url: network_config.starknet_rpc_url.clone(),
+        eth_rpc_url: network_config.eth_rpc_url.clone(),
+        l2_sender: l2_sender.to_string(),
+        starkway_l1: network_config.starkway_l1.clone(),
+        starkway_l2: network_config.starkway_l2.clone(),
+        starknet_core: network_config.starknet_core.clone(),
+    })
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
-use alloy::providers::{Provider as P, ProviderBuilder};
-use alloy::rpc::client::{ClientBuilder, RpcClient};
-use alloy::sol_types::SolCall;
-use alloy::sol_types::SolValue;
+use alloy::providers::ProviderBuilder;
+
 use alloy::{
-    primitives::{keccak256, Address, Bytes, U256},
+    primitives::{keccak256, U256},
     sol,
 };
-use chrono::{DateTime, TimeZone, Utc};
+use chrono::{TimeZone, Utc};
 use csv::Writer;
+use event_tracker::config::get_config;
 use num_bigint::BigUint;
 use starknet::{
-    core::types::{BlockId, BlockTag, EventFilter, Felt, MaybePendingBlockWithTxHashes},
-    macros::felt,
+    core::types::{BlockId, EventFilter, Felt, MaybePendingBlockWithTxHashes},
     providers::{
         jsonrpc::{HttpTransport, JsonRpcClient},
         Provider, Url,
@@ -20,39 +18,51 @@ use starknet::{
 use std::{fs::File, str::FromStr};
 
 #[tokio::main]
-async fn main() {
-    let file = File::create("output.csv").unwrap();
-    let mut wrt = Writer::from_writer(file);
-    let provider = JsonRpcClient::new(HttpTransport::new(
-        Url::parse("https://rpc.nethermind.io/mainnet-juno?apikey=5n1kZyTyMGiYmPn5YtGxlwHYSFTDRGCTGTfzFIn8nGKMdyOa").unwrap(),
-    ));
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config = get_config()?;
 
+    let file = File::create("output.csv")?;
+    let mut wrt = Writer::from_writer(file);
+    let provider = JsonRpcClient::new(HttpTransport::new(Url::parse(&config.starknet_rpc_url)?));
+
+    let l2_sender_filter = if config.l2_sender.is_empty() {
+        vec![]
+    } else {
+        vec![Felt::from_hex(&config.l2_sender).unwrap()]
+    };
     let keys = vec![
         vec![],
-        vec![],
+        l2_sender_filter,
         vec![],
         vec![Felt::from_hex("0x5749544844524157").unwrap()],
         vec![],
         vec![],
     ];
-    wrt.write_record(&["Tx Hash", "L2 Addr", "L1 Recipient", "L1 Token", "Amount"]);
+
+    wrt.write_record([
+        "Tx Hash",
+        "Timestamp",
+        "From L2 Addr",
+        "To L1 Recipient",
+        "L1 Token",
+        "Amount",
+        "Is completed on L1 (zero = yes)",
+        "Message Hash",
+    ])?;
+
     let mut cont: Option<String> = Some("initial".to_string());
+
     while cont.is_some() {
-        if cont.clone().unwrap() == "initial".to_string() {
+        if cont.clone().unwrap() == "initial" {
             cont = None;
         }
 
         let result = provider
             .get_events(
                 EventFilter {
-                    from_block: Some(BlockId::Number(657447)),
-                    to_block: Some(BlockId::Number(657486)), // 657586
-                    address: Some(
-                        Felt::from_hex(
-                            "0x3adccae1d7b4c8832133c0d090b84d4bd85f53a260dee461d51ab8dd07c9ef8",
-                        )
-                        .unwrap(),
-                    ),
+                    from_block: Some(BlockId::Number(config.from_block as u64)),
+                    to_block: Some(BlockId::Number(config.to_block as u64)), // 657586
+                    address: Some(Felt::from_hex(&config.starkway_l2)?),
                     keys: Some(keys.clone()),
                 },
                 cont.clone(),
@@ -63,62 +73,49 @@ async fn main() {
             Ok(events_page) => {
                 let emitted_events = events_page.events;
                 cont = events_page.continuation_token;
-
+                let mut index = 1;
                 for event in emitted_events {
                     let l1_recipient = event.keys[0].to_hex_string();
                     let l2_sender = event.keys[1].to_hex_string();
                     let l1_token_address = event.keys[4].to_hex_string();
                     let tx_hash = event.transaction_hash.to_hex_string();
-                    let amount = event.data[0];
+                    let amount_low = event.data[0];
                     let block_number = event.block_number.unwrap();
-                    let amount_low = amount.to_biguint();
+                    let amount_low = amount_low.to_biguint();
                     let amount_high = event.data[1].to_biguint();
                     let total_value: BigUint = amount_high << 256 | amount_low;
                     let total_value_felt = Felt::from_bytes_be_slice(&total_value.to_bytes_be());
 
-                    wrt.write_record(&[
-                        tx_hash,
-                        l2_sender.clone(),
-                        l1_recipient.clone(),
-                        l1_token_address.clone(),
-                        total_value_felt.to_string(),
-                    ]);
-
                     let result_timestamp = provider
                         .get_block_with_tx_hashes(BlockId::Number(block_number))
                         .await;
+                    let formatted: String;
+
                     match result_timestamp {
                         Ok(pending_block_data) => {
                             match pending_block_data {
                                 MaybePendingBlockWithTxHashes::Block(block_data) => {
                                     let timestamp = block_data.timestamp;
                                     // Convert timestamp to DateTime<Utc>
-                                    let datetime = Utc
-                                        .timestamp_opt(timestamp.try_into().unwrap(), 0)
-                                        .unwrap();
+                                    let datetime =
+                                        Utc.timestamp_opt(timestamp.try_into()?, 0).unwrap();
 
                                     // Format the datetime to a human-readable string
-                                    let formatted =
+                                    formatted =
                                         datetime.format("%Y-%m-%d %H:%M:%S UTC").to_string();
-
-                                    println!("Timestamp: {}", timestamp);
-                                    println!("Formatted UTC time: {}", formatted);
                                 }
                                 _ => {
-                                    panic!("Block not yet available");
+                                    formatted = "BLOCK STILL PENDING".to_string();
                                 }
                             }
                         }
                         Err(_) => {
-                            panic!("Error");
+                            formatted = "BLOCK NOT FOUND".to_string();
                         }
                     }
-                    let starkway_l2 = U256::from_str(
-                        "0x3adccae1d7b4c8832133c0d090b84d4bd85f53a260dee461d51ab8dd07c9ef8",
-                    )
-                    .unwrap();
-                    let starkway_l1 =
-                        U256::from_str("0xCAbb5DDff712598B3c8183B988f082ef3dc74E00").unwrap();
+                    let starkway_l2 = U256::from_str(&config.starkway_l2)?;
+
+                    let starkway_l1 = U256::from_str(&config.starkway_l1)?;
                     let payload = [
                         U256::from_str(&l1_token_address).unwrap(),
                         U256::from_str(&l1_recipient).unwrap(),
@@ -135,11 +132,9 @@ async fn main() {
                         packed.extend_from_slice(&item.to_be_bytes::<32>());
                     }
                     let hash = keccak256(&packed);
-                    println!("Message hash={}", hash);
+                    //println!("Message hash={}", hash);
 
-                    let rpc_url = "https://mainnet.infura.io/v3/aebb09967b724dfd91107bea3daba50f"
-                        .parse()
-                        .unwrap();
+                    let rpc_url = config.eth_rpc_url.parse()?;
 
                     // Create a provider with the HTTP transport using the `reqwest` crate.
                     let provider = ProviderBuilder::new().on_http(rpc_url);
@@ -151,27 +146,33 @@ async fn main() {
                         }
                     );
 
-                    let contract = Starknet::new(
-                        "0xc662c410C0ECf747543f5bA90660f6ABeBD9C8c4"
-                            .parse()
-                            .unwrap(),
-                        provider,
-                    );
+                    let contract =
+                        Starknet::new(config.starknet_core.as_str().parse().unwrap(), provider);
                     let num_messages = contract.l2ToL1Messages(hash);
                     let Starknet::l2ToL1MessagesReturn { _0 } = num_messages.call().await.unwrap();
-                    println!("number of messages:{_0}");
-                    // form message hash
-                    // connect to starknet core contract
-                    // check whether message consumed
-                    // get block details based on block number and get current timestamp
-                    // convert timestamp to human readable format
+                    let formatted_num_messages = format!("{_0}");
+                    wrt.write_record(&[
+                        tx_hash.clone(),
+                        formatted,
+                        l2_sender.clone(),
+                        l1_recipient.clone(),
+                        l1_token_address.clone(),
+                        total_value_felt.to_string(),
+                        formatted_num_messages,
+                        hash.to_string(),
+                    ])?;
+                    
+                    println!("Found event data for transaction {} with hash:{}", index, tx_hash);
+                    index += 1;
                 }
-                //println!("{:#?}", events_page);
+                
             }
             Err(err) => {
                 eprintln!("Error: {}", err);
             }
         }
-        wrt.flush();
+        wrt.flush()?;
     }
+
+    Ok(())
 }
